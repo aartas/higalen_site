a885edbd0239bab315f6690a2be983e3b37815d0
Rakesh jakkula <aartastech@Aartass-MacBook-Air.local>
2025-07-23 17:33:23 +0530

    Folder chnages done

--- src/app/[site_id]/layout.js
+++ src/app/[site_id]/layout.js
@@ -1,13 +1,11 @@
-export default async function siteLayout(params,children,) {
-
-    console.log('params?.site_id', params?.site_id)
-
+export default function TenantLayout({ children }) {
     return (
-        <html lang="en">
-            <body>
-                {children}
-                I am here to test the page
-            </body>
-        </html>
-    )
-}
+      <html lang="en">
+        <body className="min-h-screen font-sans bg-gray-50 text-black">
+          <header className="p-4 bg-white shadow">Public Tenant Layout</header>
+          {children}
+        </body>
+      </html>
+    );
+  }
+   
