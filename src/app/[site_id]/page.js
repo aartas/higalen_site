b6f82d518fd0b719ad71a158c543f08ba191bcd0
Rakesh jakkula <aartastech@Aartass-MacBook-Air.local>
2025-07-23 17:35:35 +0530

    Folder chnages done

--- src/app/[site_id]/page.js
+++ src/app/[site_id]/page.js
@@ -1,7 +1,7 @@
 export default function TenantHome({ params }) {
     return (
       <main className="p-6">
-        <h1 className="text-2xl font-bold">Welcome to {params.tenant}'s site!</h1>
+        <h1 className="text-2xl font-bold">Welcome to {params.tenant} tenant site!</h1>
       </main>
     );
   }
