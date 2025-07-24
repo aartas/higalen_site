8537fa5c6246932918b9c0233c8a61e5edaaa822
nikhil2802Vishwakarma <nikhil.vishwakarma@aartas.com>
2025-07-24 18:04:54 +0530

    nextjs architure changes to support multi tenant

--- src/app/[site_id]/page.js
+++
@@ -1,7 +0,0 @@
-export default function TenantHome({ params }) {
-    return (
-      <main className="p-6">
-        <h1 className="text-2xl font-bold">Welcome to {params.tenant} tenant site!</h1>
-      </main>
-    );
-  }
--- src/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-// 'use client';
-import { combineReducers } from '@reduxjs/toolkit'
-import bookingReducer from './slices/bookingActionSlice'
-
-const rootReducer = combineReducers({
-  booking: bookingReducer,
-});
-
-export default rootReducer
